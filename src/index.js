511f942242f08e1a13ce5141c48308658699ba12
tjbigelow <tjbigelow@att.net>
2020-12-21 12:13:46 -0600

    updated websocket url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import actionCable from 'actioncable'
 
 const CableApp = {}
 
-CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
+CableApp.cable = actionCable.createConsumer('ws://tjb-partygame-api.herokuapp.com/cable')
 
 ReactDOM.render(
   <React.StrictMode>

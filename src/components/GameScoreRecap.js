835bc49f14061d16f361327491b0df41418741d3
tjbigelow <tjbigelow@att.net>
2020-12-29 16:24:49 -0600

    added round counter and game over to score recap

--- src/components/GameScoreRecap.js
+++ src/components/GameScoreRecap.js
@@ -3,29 +3,37 @@ import { Table } from "react-bootstrap";
 
 export default class GameScoreRecap extends Component {
   renderScores = () => {
-    return this.props.gameData.players.sort((a,b) => {return b.score - a.score}).map((player) => {
-      return (
-        <tr key={player.id}>
-          <td style={{ color: player.isbot ? "blue" : "black" }}>
-            {player.username}
-          </td>
-          <td>{player.score}</td>
-        </tr>
-      );
-    });
+    return this.props.gameData.players
+      .sort((a, b) => {
+        return b.score - a.score;
+      })
+      .map((player) => {
+        return (
+          <tr key={player.id}>
+            <td style={{ color: player.isbot ? "blue" : "black" }}>
+              {player.username}
+            </td>
+            <td>{player.score}</td>
+          </tr>
+        );
+      });
   };
 
   render() {
+    console.log(this.props.gameData);
     return (
-      <Table className="table-striped">
-        <thead>
-          <tr>
-            <th>Players</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>{this.renderScores()}</tbody>
-      </Table>
+      <div>
+        <h2>{this.props.gameData.round === 3 ? 'Game Over' : `Score after round ${this.props.gameData.round}`}</h2>
+        <Table className="table-striped">
+          <thead>
+            <tr>
+              <th>Players</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderScores()}</tbody>
+        </Table>
+      </div>
     );
   }
 }

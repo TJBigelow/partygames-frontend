455148dd80bdc63b43ba7afc17896d438983493b
tjbigelow <tjbigelow@att.net>
2021-01-05 13:13:56 -0600

    Cleaned up game over screen

--- src/components/PlayerScoreRecap.js
+++ src/components/PlayerScoreRecap.js
@@ -5,7 +5,7 @@ export default class PlayerRecap extends Component {
     render() {
         return (
             <div>
-                {this.props.gameData.round_number === 3 ? 'Game Over' : `Waiting for Round ${this.props.gameData.round_number + 1}`}<br />
+                {this.props.gameData.round_number === 3 ? <h3>Game Over</h3> : `Waiting for Round ${this.props.gameData.round_number + 1}`}
                 {this.props.gameData.round_number === 3 ? <Button onClick={() => {this.props.setScreen({screen: "landing"})}}>Return to Home</Button> : null}
             </div>
         )
